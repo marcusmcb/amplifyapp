e6d8a0e7d4663410a99df8724aa3c8b62527eac7
Marcus McBride <marcusmcb@gmail.com>
2021-04-09 11:59:45 -0700

    added papaparse npm & dummy csv to test

--- src/components/DanceRadioChart.js
+++ src/components/DanceRadioChart.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Row } from 'react-bootstrap'
 
+import { readRemoteFile } from 'react-papaparse';
+
 const DanceRadioChart = () => {
+    readRemoteFile('https://1drv.ms/x/s!Ap_EbLI5WiBGgpBYUWzBeiLE2JViNg?e=JwBK8L', {
+        complete: (results) => {
+            console.log('Results:', results)
+        }
+    })
   return (
     <div>
       <Row>
